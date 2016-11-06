420f5dbba0b5b4f8e0e9dfda3db00a3d341386aa
Toubic <tj22fe@student.lnu.se>
2016-11-07 00:57:52 +0100

    removed duplication in american and international strategy classes

--- model/rules/AmericanNewGameStrategy.cs
+++ model/rules/AmericanNewGameStrategy.cs
@@ -9,25 +9,24 @@ namespace BlackJack.model.rules
     {
         public bool NewGame(Deck a_deck, Dealer a_dealer, Player a_player)
         {
+            getShowDealCard(a_deck, a_player);
+
+            getShowDealCard(a_deck, a_dealer);
+
+            getShowDealCard(a_deck, a_player);
+
+            getShowDealCard(a_deck, a_dealer);
+
+            return true;
+        }
+
+        public void getShowDealCard(Deck a_deck, Player a_player)
+        {
             Card c;
 
             c = a_deck.GetCard();
             c.Show(true);
             a_player.DealCard(c);
-
-            c = a_deck.GetCard();
-            c.Show(true);
-            a_dealer.DealCard(c);
-
-            c = a_deck.GetCard();
-            c.Show(true);
-            a_player.DealCard(c);
-
-            c = a_deck.GetCard();
-            c.Show(false);
-            a_dealer.DealCard(c);
-
-            return true;
         }
     }
 }
--- model/rules/INewGameStrategy.cs
+++ model/rules/INewGameStrategy.cs
@@ -8,5 +8,7 @@ namespace BlackJack.model.rules
     interface INewGameStrategy
     {
         bool NewGame(Deck a_deck, Dealer a_dealer, Player a_player);
+
+        void getShowDealCard(Deck a_deck, Player a_player);
     }
 }
